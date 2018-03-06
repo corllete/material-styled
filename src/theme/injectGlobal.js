7f8c29852b1422e9f28e0c33db91489acf278426
Veselin Totev <v.totev@corllete.com>
2018-03-06 12:45:28 +0200

    remove auto global inject

---
+++ src/theme/injectGlobal.js
@@ -0,0 +1,12 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    font-family: Roboto,sans-serif;
+  }
+`;
