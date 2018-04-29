c1ce5fa3f44554671be3c2c96c1cd89a1b1d0422
Miro Yovchev <m.yovchev@gmail.com>
2018-04-29 19:38:45 +0300

    Remove required publish version argument

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,19 +14,19 @@ if [[ ! "$update_type" =~ ^(patch|minor|major)$ ]]; then
   echo "Please specify update type - patch | minor | major"
 fi
 
-if [[ ! ${version} =~ ^(v?[0-9]+\.[0-9]+\.[0-9]+)(-.*)?$ ]] ; then
-  error=1
-  echo "Please specify valid git tag (semantic versioning standard)"
-fi
+# if [[ ! ${version} =~ ^(v?[0-9]+\.[0-9]+\.[0-9]+)(-.*)?$ ]] ; then
+#   error=1
+#   echo "Please specify valid git tag (semantic versioning standard)"
+# fi
 
 if [[ ! -z "$error" ]]; then
   echo
   echo "Usage: "
-  echo "./publish.sh patch|minor|major [v]XX.XX.XX[-build|alpha|beta.XXX]"
+  echo "./publish.sh patch|minor|major"
 else
-  if [[ ${version:0:1} != "v" ]]; then
-      version="v${version}"
-  fi
+  # if [[ ${version:0:1} != "v" ]]; then
+  #     version="v${version}"
+  # fi
 
   echo "1. [build] yarn build:prod"
   yarn build:prod > /dev/null
